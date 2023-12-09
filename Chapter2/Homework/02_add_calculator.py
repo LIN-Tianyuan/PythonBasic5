e2486006e65308ef737130c6d7e9870bb2541d28
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2023-12-09 15:32:40 +0100

    Week 5

---
+++ Chapter2/Homework/02_add_calculator.py
@@ -0,0 +1,4 @@
+number1 = int(input("Please enter the number1: "))
+number2 = int(input("Please enter the number2: "))
+sum = number1 + number2
+print("The sum of two number is: " + str(sum) + ".")
--- Chapter2/Project/apple.py
+++ Chapter2/Project/apple.py
@@ -1,6 +1,4 @@
 price_str = input("Veuillez entrer le prix des pommes: ")
-print(price_str)
 weight_str = input("Veuillez entrer le poids des pommes: ")
-print(weight_str)
-money_str = int(weight_str) * int(price_str)
-print(money_str)
+money_int = int(weight_str) * int(price_str)
+print("Vous devez payer " + str(money_int) + " euros.")
